419840cf302981dc8e31289228a6749d18c30914
Eric Stevens <ericjaystevens@gmail.com>
2021-08-11 22:09:45 -0400

    test ci/cd

--- HttpExample.cs
+++ HttpExample.cs
@@ -27,7 +27,7 @@ namespace backend
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                : $"Hello, {name} Let's do this!";
 
             return new OkObjectResult(responseMessage);
         }
